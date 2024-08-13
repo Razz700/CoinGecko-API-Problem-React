15de8e733f1140e6217984ff5c5c23b8d2bed6dd
Razzakh <mrazzakh123@gmail.com>
2024-08-13 12:40:58 +0530

    first

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -56,7 +56,7 @@ if (check==='mktcap') {
   <thead>
     <tr>{heading.map((item,i)=><th key={'heading'+i}>{item}</th>)}</tr>
   </thead>
-  {!data && <p>Loading...</p>}
+  {!data && <tr><td colSpan={8} style={{textAlign:'center'}}>Loading...</td></tr>}
   {data && <tbody>
   {newData.map((item,i)=><tr key={'coin'+i}>
     <td><img src={item.image} style={{width:'2rem',height:'2rem'}} alt='coin-img' /></td>
