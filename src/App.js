ba7b0b9f9600e1e93fec6466e6c1aba001825383
Razzakh <mrazzakh123@gmail.com>
2024-08-22 18:08:57 +0530

    one

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
    useEffect(()=>{
 fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false').then((res)=>res.json()).then((datavalue)=>{
-  console.log(datavalue);
+ // console.log(datavalue);
   setData(datavalue);
 }).catch((e)=>console.log(e));
   },[]);
